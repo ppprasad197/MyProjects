de73658b95e11708fec5b73180d6aacbf4685317
ppprasad197 <prasadpotdar197@gmail.com>
2025-03-31 20:21:46 +0530

    initial commit

---
+++ postandcomment/src/main/java/com/postandcomments/postandcomment/controller/CommentController.java
@@ -0,0 +1,8 @@
+package com.postandcomments.postandcomment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+
+}
---
+++ postandcomment/src/test/java/com/postandcomments/postandcomment/PostandcommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postandcomments.postandcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostandcommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
